b8ca97c128df6d6df0fdea3b720a8e8d4b5c31d0
zottaa <zottaa691@gmail.com>
2024-01-13 07:27:14 +0700

    finish task041-comparable

--- app/src/main/java/com/github/johnnysc/practicetdd/ListComparable.kt
+++ app/src/main/java/com/github/johnnysc/practicetdd/ListComparable.kt
@@ -4,14 +4,17 @@ interface ListComparable {
     fun areItemsTheSame(other: ListComparable): Boolean
     fun areContentsTheSame(other: ListComparable): Boolean
 
-    class Base(
+    fun <T : Any> compareIds(id: T): Boolean
+
+    data class Base(
         private val id: String,
         private val name: String
     ) : ListComparable {
-        override fun areItemsTheSame(other: ListComparable) =
-            id == (other as Base).id
+        override fun areItemsTheSame(other: ListComparable) = other.compareIds(id)
 
-        override fun areContentsTheSame(other: ListComparable) =
-            name == (other as Base).name
+        override fun areContentsTheSame(other: ListComparable) = this == other
+
+        override fun <T : Any> compareIds(id: T): Boolean =
+            this.id == id
     }
 }
